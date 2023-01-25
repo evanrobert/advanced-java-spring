e556847f83fe0bec3d9b86abd87ac32f3a2588e1
evan <evan.lindsay223@gmail.com>
2023-01-24 23:28:23 -0800

    finished commenting assignment @LAZY

--- src/main/java/platform/codingnomads/co/corespring/examples/lazyannotation/LazyAnnotationDemo.java
+++ src/main/java/platform/codingnomads/co/corespring/examples/lazyannotation/LazyAnnotationDemo.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class LazyAnnotationDemo {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-        ctx.register(LazyAnnotationDemoConfiguration.class);
+      //  ctx.register(LazyAnnotationDemoConfiguration.class);
         ctx.refresh();
         ctx.start();
-        ctx.getBean(LazyBean.class);
+      //  ctx.getBean(LazyBean.class);
         ctx.close();
     }
 }
