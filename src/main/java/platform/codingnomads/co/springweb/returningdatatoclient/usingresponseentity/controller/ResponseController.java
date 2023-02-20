26d46338ae79842092257b24ad4b3e71f56fbbae
evan <evan.lindsay223@gmail.com>
2023-02-20 00:18:10 -0800

    returning data completed

--- src/main/java/platform/codingnomads/co/springweb/returningdatatoclient/usingresponseentity/controller/ResponseController.java
+++ src/main/java/platform/codingnomads/co/springweb/returningdatatoclient/usingresponseentity/controller/ResponseController.java
@@ -1,10 +1,12 @@
 package platform.codingnomads.co.springweb.returningdatatoclient.usingresponseentity.controller;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import platform.codingnomads.co.springweb.returningdatatoclient.usingresponseentity.model.User;
 
@@ -14,13 +16,35 @@ import java.net.URI;
 public class ResponseController {
 
     User user = new User(1, "Test User", "test@email.com");
+    User user2 = new User(5,"Bill","Bill@gmail");
 
-    @GetMapping("/constructor")
+
+    @GetMapping("/constructor/practice")
     public ResponseEntity<User> constructorMethod() {
         HttpHeaders headers = new HttpHeaders();
         headers.add("TEST", "TEST HEADER");
         headers.add("Location", "/users/" + user.getId());
         return new ResponseEntity<>(user, headers, HttpStatus.CREATED);
+    }
+    @GetMapping("/demo")
+    User getUser(){
+        return user;
+    }
+   @GetMapping("/demo2")
+
+    public ResponseEntity<User> addUser(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("adding USER","TEST"+ user2.getId());
+        return  new ResponseEntity<>(user2,httpHeaders,HttpStatus.CREATED);
+
+
+    }
+    @GetMapping("/link/user")
+    public ResponseEntity<User> construct(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("HEADER V1","HEADER version 1" + user.getName());
+       // return  new ResponseEntity<>(user,headers,HttpStatus.ACCEPTED);
+        return  new ResponseEntity<>(user, headers,HttpStatus.FORBIDDEN);
     }
 
     @GetMapping("/builder")
@@ -37,5 +61,7 @@ public class ResponseController {
         } else {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
+
+
     }
 }
