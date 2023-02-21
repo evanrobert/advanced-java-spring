b3eb30fd6855c6e5ea977a43434143c985a05029
evan <evan.lindsay223@gmail.com>
2023-02-20 17:02:30 -0800

    added a liat of users and returned them using responsebody and controller

--- src/main/java/platform/codingnomads/co/springweb/returningdatatoclient/responsebody/controller/UserController.java
+++ src/main/java/platform/codingnomads/co/springweb/returningdatatoclient/responsebody/controller/UserController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import platform.codingnomads.co.springweb.returningdatatoclient.responsebody.model.User;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 @Controller
 public class UserController {
 
@@ -16,6 +20,11 @@ public class UserController {
             .name("Spring Dev")
             .email("dev@codingnomads.co")
             .build();
+    public User user2 = User.builder().id(2000).name("bill").email("billsemail@email.com").build();
+
+
+    public User user3 = User.builder().id(2500).name("jill").email("jillssemail@email.com").build();
+    public User user4 = User.builder().id(3000).name("will").email("willssemail@email.com").build();
 
     //using ResponseBody to return a POJO
     @ResponseBody
@@ -36,4 +45,17 @@ public class UserController {
         return user;
     }
 
+
+    @ResponseBody
+    @GetMapping("/user/list")
+    public List<User> userList() {
+        List<User> users = new ArrayList<>();
+
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        return users;
+
+
+    }
 }
